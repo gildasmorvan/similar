0c172e3ecef865279a3a05d4f78d77ec8c57a323
Kubera Yoann <yoann.kubera@univ-artois.fr>
2014-01-15 15:56:17 +0100

    Updated the documentation with micro kernel guidelines for the design of a simulation model (up to level skeleton definition)

---
+++ src/doc/microKernel/files/simulationModel/preparation/IRandomValuesGenerator.java
@@ -0,0 +1,21 @@
+package fr.lgi2a.mysimulation.model;
+
+/**
+ * Defines the random value generation methods that can be used in this simulation.
+ */
+public interface IRandomValuesGenerator {
+	/**
+	 * Gets a random number between 0 (included) and 1 (excluded).
+	 * @return A random number between 0 (included) and 1 (excluded).
+	 */
+	double randomDouble( );
+
+	/**
+	 * Gets a random integer value between 0 (included) and the specified 
+	 * higher bound (excluded).
+	 * @param higherBound The higher bound used for random integer generation.
+	 * @return a random integer value between 0 (included) and 
+	 * <code>higherBound</code> (excluded).
+	 */
+	int randomInt( int higherBound );
+}
---
+++ src/doc/microKernel/files/simulationModel/preparation/MyParametersClass.java
@@ -0,0 +1,56 @@
+package fr.lgi2a.mysimulation.model;
+
+/**
+ * Models the parameters used in this simulation.
+ */
+public class MyParametersClass {
+	/**
+	 * Defines the initial number of agents.
+	 */
+	private int initialNumberOfAgents;
+	/**
+	 * Defines the initial speed of an agent.
+	 */
+	private double agentInitialSpeed;
+
+	/**
+	 * Builds a parameters set containing default values.
+	 * The values can be changed using the setters of the parameters.
+	 */
+	public MyParametersClass( ) {
+		this.initialNumberOfAgents = 350;
+		this.agentInitialSpeed = 25;
+	}
+
+	/**
+	 * Gets the initial number of agents.
+	 * @return The initial number of agents.
+	 */
+	public int getInitialNumberOfAgents( ){
+		return this.initialNumberOfAgents;
+	}
+
+	/**
+	 * Sets the initial number of agents.
+	 * @param initialNumberOfAgents The initial number of agents.
+	 */
+	public void setInitialNumberOfAgents( int initialNumberOfAgents ){
+		this.initialNumberOfAgents = initialNumberOfAgents ;
+	}
+
+	/**
+	 * Gets the initial speed of an agent.
+	 * @return The initial speed of an agent.
+	 */
+	public double getAgentInitialSpeed( ) {
+		return this.agentInitialSpeed;
+	}
+
+	/**
+	 * Sets the initial speed of an agent.
+	 * @param agentInitialSpeed The initial speed of an agent.
+	 */
+	public void setAgentInitialSpeed( double agentInitialSpeed ){
+		this.agentInitialSpeed  = agentInitialSpeed ;
+	}
+}

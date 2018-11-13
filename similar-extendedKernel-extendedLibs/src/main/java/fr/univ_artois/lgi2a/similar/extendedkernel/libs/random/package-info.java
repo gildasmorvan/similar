1ed714724adbd656c9f83970d7a74d020585f04b
Gildas Morvan <gildas.morvan@univ-artois.fr>
2018-11-13 13:29:02 +0100

    move PRNG from similar2logo to similar

---
+++ similar-extendedKernel-extendedLibs/src/main/java/fr/univ_artois/lgi2a/similar/extendedkernel/libs/random/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author <a href="http://www.lgi2a.univ-artois.fr/~morvan" target="_blank">Gildas Morvan</a>
+ *
+ */
+package fr.univ_artois.lgi2a.similar.extendedkernel.libs.random;

1d616c8a86bd2d9c396a9ed55b8f0130bc7cb220
Gildas Morvan <gildas.morvan@univ-artois.fr>
2019-11-26 13:58:20 +0100

    Fix doc

--- similar-microKernel-commonLibs/src/main/java/fr/univ_artois/lgi2a/similar/microkernel/libs/engines/EngineMultithreadedDefaultdisambiguation.java
+++ similar-microKernel-commonLibs/src/main/java/fr/univ_artois/lgi2a/similar/microkernel/libs/engines/EngineMultithreadedDefaultdisambiguation.java
@@ -59,9 +59,11 @@ import fr.univ_artois.lgi2a.similar.microkernel.libs.tools.engine.AbstractMultit
 import fr.univ_artois.lgi2a.similar.microkernel.libs.tools.engine.DynamicStateMap;
 
 /**
- * Models a simulation engine using monothreaded algorithms and the default disambiguation mechanism.
+ * Models a simulation engine using multithreaded algorithms and the default disambiguation mechanism.
  * 
  * @author <a href="http://www.yoannkubera.net" target="_blank">Yoann Kubera</a>
+ * @author <a href="http://www.lgi2a.univ-artois.fr/~morvan" target="_blank">Gildas Morvan</a>
+ * 
  */
 public class EngineMultithreadedDefaultdisambiguation extends AbstractMultithreadedEngine {
 	/**
